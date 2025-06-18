43d3260b21e478a5822dde44834bba6dfdaef0f9
L1nkGr <96806461+00ye@users.noreply.github.com>
2025-06-18 15:14:53 +0800

    更新 members.ts

--- members.ts
+++ members.ts
@@ -9,5 +9,28 @@ export const members: Member[] = [
     avatarSrc: "/avatars/l1nkGr.png",
     sources: ["https://blog.000k.de/rss"],
   },
-  
+  {
+    id: "L1nKGr",
+    name: "L1nKGr",
+    role: "Researcher",
+    bio: "深呼吸的时候，就能保持沉稳",
+    avatarSrc: "/avatars/l1nkGr.png",
+    sources: ["https://blog.000k.de/rss"],
+  },
+  {
+    id: "L1nKGr",
+    name: "L1nKGr",
+    role: "Researcher",
+    bio: "深呼吸的时候，就能保持沉稳",
+    avatarSrc: "/avatars/l1nkGr.png",
+    sources: ["https://blog.000k.de/rss"],
+  },
+  {
+    id: "L1nKGr",
+    name: "L1nKGr",
+    role: "Researcher",
+    bio: "深呼吸的时候，就能保持沉稳",
+    avatarSrc: "/avatars/l1nkGr.png",
+    sources: ["https://blog.000k.de/rss"],
+  },
 ];
