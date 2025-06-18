84beccb53795ebf8e4c945cb4e99add2c0a4fed6
L1nkGr <96806461+00ye@users.noreply.github.com>
2025-06-18 21:14:38 +0800

    修改about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -12,11 +12,14 @@ const Page: NextPage = () => {
           <h1 className="about__title">About</h1>
           <div className="about__body">
             <p>
-              このサイトはチームのためのブログスターター
+            本仓库属于Dgut hacktb实验室创建的博客聚合平台，欢迎实验室成员参与进来
               <a href="https://hacktb.com">
               hacktb Team Blog Hub
               </a>
-              のデモです。ブログのRSSのURLを登録することで、チームメンバーの投稿を一覧にまとめて表示します。
+              <br/>
+              <a>目前负责维护本仓库:l1nkGr,</a>
+              <br/>
+              <a>项目仓库:https://github.com/00ye/team-blog-hub,欢迎合并加入</a>
             </p>
           </div>
           
