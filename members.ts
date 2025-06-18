9d0e22f485dd96bc3901a3bd4c770033d9e1962a
L1nkGr <96806461+00ye@users.noreply.github.com>
2025-06-18 14:33:56 +0800

    更新 members.ts

--- members.ts
+++ members.ts
@@ -6,15 +6,8 @@ export const members: Member[] = [
     name: "L1nKGr",
     role: "Researcher",
     bio: "深呼吸的时候，就能保持沉稳",
-    avatarSrc: "/avatars/naoko.jpg",
+    avatarSrc: "/avatars/l1nkGr.png",
     sources: ["https://blog.000k.de/rss"],
   },
-  {
-    id: "alexandria",
-    name: "Alexandria",
-    role: "Tech Lead",
-    bio: "IT professional with 3 years of experience",
-    avatarSrc: "/avatars/alexandria.jpg",
-    sources: [],
-  },
+  
 ];
