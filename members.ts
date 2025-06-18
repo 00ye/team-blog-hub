718092a202e2d7a91088d82cb8dc44717ae58330
L1nkGr <96806461+00ye@users.noreply.github.com>
2025-06-18 16:17:42 +0800

    添加大家的链接

--- members.ts
+++ members.ts
@@ -2,12 +2,155 @@ import { Member } from "@src/types";
 
 export const members: Member[] = [
   {
-    id: "L1nKGr",
-    name: "L1nKGr",
-    role: "Researcher",
-    bio: "深呼吸的时候，就能保持沉稳",
-    avatarSrc: "/avatars/l1nkGr.png",
-    sources: ["https://blog.000k.de/rss"],
+    id: "林梓钘",
+    name: "林梓钘 老师",
+    role: "HackTB 实验室主要负责老师，现任东莞理工学院计算机学院网安系副系主任，主要研究方向是隐私保护与制造数据安全",
+    bio: "后生可畏，未来可期",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-19_215200_183.png",
+    sources: ["https://www.cnblogs.com/lynnzixing"]
   },
-  
+  {
+    id: "张刘飘",
+    name: "张刘飘 老师",
+    role: "HackTB 实验室负责老师，密码学博士，硕士生导师，主要研究方向序列密码，安全检索。",
+    bio: "学好安全技术，守好安全底线，网安之路，未来可期",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-20_194553_830.jpg",
+    sources: []
+  },
+  {
+    id: "黄耀康",
+    name: "黄耀康",
+    role: "实验室创始人，dgut纯血打工吗楼",
+    bio: "祝大家在网安的知识里汲取到实用的知识！",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-19_223111_415.jpg",
+    sources: ["https://home.cnblogs.com/u/lisenMiller"]
+  },
+  {
+    id: "黄锦明",
+    name: "黄锦明",
+    role: "无情的文档输出机器",
+    bio: "冲鸭！大手子们",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-19_223116_767.jpg",
+    sources: ["https://www.cnblogs.com/jimmy-hwang"]
+  },
+  {
+    id: "吕卓成",
+    name: "吕卓成",
+    role: "src高手，擅长妙妙工具开发",
+    bio: "擅长各种摸鱼手段，闭关摸鱼中...",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-19_223032_912.jpg",
+    sources: ["https://endermanneer.github.io"]
+  },
+  {
+    id: "周家锋",
+    name: "周家锋",
+    role: "CTF竞赛Web方向大佬，擅长挖洞",
+    bio: "不好，我被大佬包围了",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-19_223054_481.jpg",
+    sources: ["https://icewindy.cn"]
+  },
+  {
+    id: "王科城",
+    name: "王科城",
+    role: "沉淀",
+    bio: "共 &ensp;&ensp;&ensp; 相<br>同 &ensp;&ensp;&ensp; 互<br>进 &ensp;&ensp;&ensp; 吹<br>步 &ensp;&ensp;&ensp; 捧",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-19_232058_254.jpg",
+    sources: ["https://hihopkc.github.io"]
+  },
+  {
+    id: "林麟坤",
+    name: "林麟坤",
+    role: "基于web、misc生活的黑客大佬",
+    bio: "深呼吸的时候，就能保持沉稳",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-19_231753_226.jpg",
+    sources: ["https://t00.ls/0linkun"]
+  },
+  {
+    id: "唐高宇",
+    name: "唐高宇",
+    role: "DGUT::Cyber 搭建、维护牛马，主攻二进制安全",
+    bio: "( •̀ ω •́ )✧",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-19_223324_482.jpg",
+    sources: ["https://www.cnblogs.com/resea"]
+  },
+  {
+    id: "肖锐浩",
+    name: "肖锐浩",
+    role: "V&N联合战队成员、edusrc正式白帽子",
+    bio: "补天src低手，主要方向web+misc，赛棍一枚",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-19_223134_648.jpg",
+    sources: ["https://www.cnblogs.com/xhzccy"]
+  },
+  {
+    id: "罗宗毅",
+    name: "罗宗毅",
+    role: "渗透似呼吸 挖洞若饮溪",
+    bio: "上下左右都是大佬除了我",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-19_223125_127.jpg",
+    sources: ["https://hustler0000.github.io"]
+  },
+  {
+    id: "张文崧",
+    name: "张文崧",
+    role: "js逆向、代码审计、爬虫大佬，博客质量超高",
+    bio: "大家好我是渗透小菜b",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-19_223144_951.jpg",
+    sources: ["https://www.cnblogs.com/wssw"]
+  },
+  {
+    id: "黄廷翰",
+    name: "黄廷翰",
+    role: "musc爱好者",
+    bio: "%df%5c",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-19_235208_140.jpg",
+    sources: ["https://www.cnblogs.com/ethereal258"]
+  },
+  {
+    id: "何俊龙",
+    name: "何俊龙",
+    role: "DGUT_Sec补天团队成员、src水洞终结者",
+    bio: "在榜的都是大佬，只有我是小萌新",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-20_093256_420.jpg",
+    sources: ["https://www.cnblogs.com/Small-Dragon"]
+  },
+  {
+    id: "曹博学",
+    name: "曹博学",
+    role: "全栈开发者，偶尔搞逆向，爱搞爬虫和脚本",
+    bio: "啥都沾点",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-20_194350_882.jpg",
+    sources: ["https://github.com/haojiezhe12345"]
+  },
+  {
+    id: "朱文锋",
+    name: "朱文锋",
+    role: "Web爱好者、HackTheBoxEasy靶场终结者",
+    bio: "好多东西要学学不过来遂开摆",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-20_195128_618.jpg",
+    sources: ["https://www.cnblogs.com/QiSamaQwQ"]
+  },
+  {
+    id: "陈俊全",
+    name: "陈俊全",
+    role: "src爱好者，java安全死对头",
+    bio: "echo %0|%0 >A.bat&.\\A.bat",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-04-20_195249_517.jpg",
+    sources: ["https://gitee.com/a19983"]
+  },
+  {
+    id: "邱振轩",
+    name: "邱振轩",
+    role: "逆向牢玩家，又菜又爱玩。",
+    bio: "叫我做咩～",
+    avatarSrc: "/img/tbTeamMembers/0eaa45fb9b0128acbadfb9b6817ef4fa.jpg",
+    sources: ["https://www.cnblogs.com/ClownLMe"]
+  },
+  {
+    id: "黄龙腾",
+    name: "黄龙腾",
+    role: "苦逼安服仔一枚",
+    bio: "从入门到入狱",
+    avatarSrc: "/img/tbTeamMembers/微信图片_2025-06-18_160054_533.jpg",
+    sources: ["honestlin.top"]
+  }
 ];
